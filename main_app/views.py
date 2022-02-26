5bd86daa2681861f444e5f0b9c818d1a1d06f11e
TonyPurple <tony.vanoni@outlook.com>
2022-02-26 11:39:50 -0800

    add model form

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Add the following import
 from django.http import HttpResponse
 
+from .forms import WidgetForm
+from .models import Widget
+
 # Define the home view
 def index(request):
-  return render(request, 'index.html')
+    widgets = Widget.objects.all()
+    widget_form = WidgetForm()
+    return render(request, 'index.html', {
+        "widget_form": widget_form,
+        "widget_list": widgets,
+    })
+
+def create(request):
+    form = WidgetForm(request.POST)
+    if form.is_valid():
+        add_widget = form.save(commit=False)
+        add_widget.save()
+    widgets = Widget.objects.all()
+    widget_form = WidgetForm()
+    return redirect('/', {
+        "widget_form": widget_form,
+        "widget_list": widgets,
+    })
